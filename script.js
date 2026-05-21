527995f008ac109e88231682b9ce9d1f2daee721
Nobyl <99382278+Nobyl@users.noreply.github.com>
2026-05-20 23:07:44 -0400

    Update ids, Add character sheet cards

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
 	$(document).on('click', '.bs-canvas-pull-button', function() {
-		$('body').prepend('<div class="bs-canvas-overlay bg-dark position-fixed w-100 h-100"></div>');
-
 		var canvasPullButtonId = $(this)[0].id;
+
+		$('body').prepend('<div class="bs-canvas-overlay bg-dark position-fixed w-100 h-100"></div>');
 		
 		switch(canvasPullButtonId) {
-			case "character-sheet-button-left" :
-				$('#character-sheet-canvas-left').addClass('ml-0');
+			case "characterSheetButtonLeft" :
+				$('#characterSheetCanvasLeft').addClass('ml-0');
 				break;
-			case "character-sheet-button-right" :
-				$('#character-sheet-canvas-right').addClass('mr-0');
+			case "characterSheetButtonRight" :
+				$('#characterSheetCanvasRight').addClass('mr-0');
 				break;
-			case "damage-calculator-button-left" :
-				$('#damage-calculator-canvas-left').addClass('ml-0');
+			case "damageCalculatorButtonLeft" :
+				$('#damageCalculatorCanvasLeft').addClass('ml-0');
 				break;
-			case "damage-calculator-button-right" :
-				$('#damage-calculator-canvas-right').addClass('mr-0');
+			case "damageCalculatorButtonRight" :
+				$('#damageCalculatorCanvasRight').addClass('mr-0');
 				break;
 			default :
 		}
@@ -25,11 +25,9 @@ jQuery(document).ready(function($){
 	});
 	
 	$(document).on('click', '.bs-canvas-close, .bs-canvas-overlay', function() {
-
 		var pullCanvas = $(this).hasClass('bs-canvas-close') ? $(this).closest('.bs-canvas') : $('.bs-canvas');
 
 		pullCanvas.removeClass('mr-0 ml-0');
-
 		$('.bs-canvas-overlay').remove();
 
 		return false;
